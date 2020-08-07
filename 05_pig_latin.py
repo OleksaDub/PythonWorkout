be86ea43729c655c7ee3d5a7390c63440111ccc3
OleksaDub <alex.dubrovsky@yahoo.com>
2020-08-07 22:34:04 +0300

    05 Pig Latin capitalization

--- 05_pig_latin.py
+++ 05_pig_latin.py
@@ -13,4 +13,17 @@ def pig_latin(word):
         
     return res
         
-print(pig_latin('python'))
+#print(pig_latin('python'))
+
+# Handle caitalisation
+def pl_capitalization(word):
+    if not word[0].isupper():
+        res = pig_latin(word)
+    else:
+        newWord = word[0].lower() + word[1:]
+        res = pig_latin(newWord).capitalize()
+        
+    return(res)
+        
+
+print(pl_capitalization("Link"))
